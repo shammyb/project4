008683095d76f31a46b183bfa48cae14d3377fec
Katherine Herbert <katherinekherbert@gmail.com>
2021-03-17 11:25:30 +0100

    form error handling

--- project4/client/components/UpdatePost.js
+++ project4/client/components/UpdatePost.js
@@ -1,13 +1,23 @@
-//import React from 'react'
-import React, { useState } from 'react'
-import axios from 'axios'
+// import React from 'react'
+// // import ReactDOM from 'react-dom'
+// import { Formik, Form, useField } from 'formik'
+// import * as Yup from 'yup'
+// import axios from 'axios'
+
+import React from 'react'
+// import React, { useState } from 'react'
+// import axios from 'axios'
+
+// function UpdatePost({ history, match }) {
+// const postId = match.params.postId
+
 
 function UpdatePost() {
- 
+
   return <div>
     <h1>Update Post</h1>
   </div>
-  
+
 
 }
 
